41c680ce68cb0d6c33b2c560feec4bd967942297
anthony-gubler <anthony.gubler@thomsonreuters.com>
2014-05-11 11:00:18 +0100

    Skip retweets

--- app.js
+++ app.js
@@ -22,22 +22,26 @@ if (program.user) {
 
 	stream.on('tweet', function (tweet) {
 		console.log("Registered Tweet", tweet);
-		if (!tweet.in_reply_to_screen_name && !tweet.in_reply_to_status_id && !tweet.in_reply_to_user_id) {
-			if (tweet.text.length < 128) {
-				if (!!program.debug) {
-					twit.post('statuses/update', { status: "Dear Diary " + tweet.text}, function (err, details) {
-						if (err) {
-							console.log("Error posting tweet", tweet.text);
-						}
-					});
+		if (!tweet.retweeted_status) {
+			if (!tweet.in_reply_to_screen_name && !tweet.in_reply_to_status_id && !tweet.in_reply_to_user_id) {
+				if (tweet.text.length < 128) {
+					if (!!program.debug) {
+						twit.post('statuses/update', { status: "Dear Diary " + tweet.text}, function (err, details) {
+							if (err) {
+								console.log("Error posting tweet", tweet.text);
+							}
+						});
+					} else {
+						console.log("Dry-run tweet details:", "Dear Diary", tweet.text);
+					}
 				} else {
-					console.log("Dry-run tweet details:", "Dear Diary", tweet.text);
+					console.log("Potential Diary tweet missed due to length", tweet.text);
 				}
 			} else {
-				console.log("Potential Diary tweet missed due to length", tweet.text);
+				console.log("Tweet was a reply", tweet.text);
 			}
 		} else {
-			console.log("Tweet was a reply", tweet.text);
+			console.log("Tweet was a retweet");
 		}
 	});
 	console.log("Application Started");
