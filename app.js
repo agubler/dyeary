8ae3dfbc9932d96ae9998b9186b0530a23556478
anthony-gubler <anthony.gubler@thomsonreuters.com>
2014-05-11 10:56:09 +0100

    added dry run

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 var Twit = require("twit");
 var program = require('commander');
 
-program.version("0.0.1").option("-u, --user <value>", "Twitter screen name or user Id to track").parse(process.argv);
+program
+	.version("0.0.1")
+	.option("-u, --user <value>", "Twitter screen name or user Id to track")
+	.option("-d, --dryrun", "Set dryrun on, not tweets will be posted")
+	.parse(process.argv);
 
 try {
 	var authConfig = require("./authConfig");
@@ -20,11 +24,15 @@ if (program.user) {
 		console.log("Registered Tweet", tweet);
 		if (!tweet.in_reply_to_screen_name && !tweet.in_reply_to_status_id && !tweet.in_reply_to_user_id) {
 			if (tweet.text.length < 128) {
-				twit.post('statuses/update', { status:  "Dear Diary " + tweet.text}, function (err, details) {
-					if (err) {
-						console.log("Error posting tweet", tweet.text);
-					}
-				});
+				if (!!program.debug) {
+					twit.post('statuses/update', { status: "Dear Diary " + tweet.text}, function (err, details) {
+						if (err) {
+							console.log("Error posting tweet", tweet.text);
+						}
+					});
+				} else {
+					console.log("Dry-run tweet details:", "Dear Diary", tweet.text);
+				}
 			} else {
 				console.log("Potential Diary tweet missed due to length", tweet.text);
 			}
